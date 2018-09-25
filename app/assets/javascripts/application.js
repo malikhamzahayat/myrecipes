644449eecb7fcd618110d5e19675ad6d52ab71e6
Hamza Hayat <malikhamzahayat@ucp.edu.pk>
2018-09-26 00:01:09 +0500

    install gem jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery-ujs
 //= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
